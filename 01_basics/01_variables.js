9a3888a3796bb36ac01c128e99ef827e5433b63e
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2025-03-05 00:16:14 +0530

    3. Conversion Operations

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 456786;
+let accountEmail ="jatin@gmail.com";
+var accountPassword = "14526";
+accountCity="Guna";
+console.log(accountId, accountEmail, accountPassword, accountCity);
+accountEmail= "jatinjoshi@gmail.com";
+console.log(accountEmail);
+console.table({accountId, accountEmail, accountPassword, accountCity});
+// we are not going to use var keyword in our code beacuse it is not block scoped and we'll use let and const instead of var.
