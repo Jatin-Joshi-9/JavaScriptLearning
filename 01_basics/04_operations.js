b2c2f462dd2d6b8e36294377ab171cbc4f6f49d0
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2025-03-05 00:27:47 +0530

    operations completed

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,42 @@
+// //************************** Opearations ************************************ */
+// let value =3;
+// let negativevalue = -value;
+// console.log(negativevalue);
+// console.log(value + 3);
+// console.log(value - 3);
+// console.log(value * 3);
+// console.log(value / 3);
+// console.log(value % 3);
+// console.log(value ** 3);
+// console.log(value++);
+// console.log(value--);
+// console.log(++value);
+// console.log(--value);
+// console.log(value+=3);
+// console.log(value-=3);
+// console.log(value*=3);
+// console.log(value/=3);
+// console.log(value%=3);
+// console.log(value**=3);
+// console.log(value);
+
+
+// //************************** String Concatenation ************************************ */
+// String concatenation
+// let str1="Hello";
+// let str2="World";
+// let str3=str1+str2;
+// console.log(str3);
+
+// console.log(1+ str1);
+// console.log(str1+1);
+// console.log(1+1+str1);
+// console.log(str1+1+1);
+// Above four lines will give different output because of the order of the operands
+
+
+// console.log(+true); //is possible but not recommended
+// console.log(+false); //is possible but not recommended
+
+
+//Our code should be readable and maintainable. So, we should avoid using + operator for type conversion.
