78b8b97f8db9e956d84aef3038fc9828710f1ea9
Jatin-Joshi-9 <jatinjoshi090803@gmail.com>
2025-03-06 00:12:33 +0530

    Updated String:  input

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -1,3 +1,25 @@
  const myname= new String("Jatin Joshi");
- console.log(myname[5]);
- console.log(myname.length);
+//  console.log(myname[5]);
+//  console.log(myname.length);
+
+//  console.log(myname.toUpperCase());
+//  console.log(myname.toLowerCase()); 
+//  console.log(myname.charAt(7));
+//  console.log(myname.indexOf('si'));
+//  console.log(myname.lastIndexOf('J'));
+ 
+//  let str=myname.slice(-7,5);
+//  console.log(str);
+
+//  let newStringone=" jatin has a book which has a cover    ";
+//  console.log(newStringone.trim());// Trim removes the extra spaces from the string
+//  newStringone=newStringone.trim();
+//  console.log(newStringone.startsWith('jatin'));
+
+
+const url ="https://www.google.com";
+console.log(url.replace(' ',"o"));
+//To follow the link below
+
+const stringarr=url.split('');
+console.log(typeof stringarr);
